ec60ecad740efb052c542a750bde9ec57676bc14
Paul Raestad <Paul Raestad>
2017-04-30 23:20:37 +0100

    Tboard::set_val() og Tboard::get_val()

--- Tboard.h
+++ Tboard.h
@@ -1,6 +1,20 @@
 #include "Grid.h"
 
-class Tboard: public Grid {
+class Tboard: public Grid<Rute> {
 	
-	
+	public:
+		Tboard(int sz) : Grid(sz) {}
+		Rute_verdi set_val(int x, int y, Rute_verdi rv);
+		Rute_verdi get_val(int x, int y);
 };
+
+Rute_verdi Tboard::set_val(int x, int y, Rute_verdi rv) {
+	
+	
+	
+}
+
+Rute_verdi Tboard::get_val(int x, int y) {
+	
+	
+}
